5301ac5700790d4fb1bf241be67f18bab5d720a7
Mark Otto <markdotto@gmail.com>
2022-05-15 19:55:21 -0700

    wip

---
+++ sass-js/js/main.js
@@ -0,0 +1,10 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+//
+// Place any custom JS here
+//
