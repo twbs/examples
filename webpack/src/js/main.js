ea9fcbf513d1ecbcdfd4f90a0e8d7939292b76d4
allen-frye <116049759+allen-frye@users.noreply.github.com>
2023-02-04 20:37:33 -0600

    Added Hangman - sass issues

--- webpack/src/js/main.js
+++ webpack/src/js/main.js
@@ -1,6 +1,6 @@
 // Import our custom CSS
 import '../scss/styles.scss';
-
+// import "../scss/_hangman.scss";
 // Import only the Bootstrap components we need
 import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
 
@@ -9,3 +9,5 @@ document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new Popover(popover)
   })
+
+import './hangman.js';
