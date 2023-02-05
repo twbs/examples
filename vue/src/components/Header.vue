9f96475814ababc12ee93c0ba27522cf40acad41
Julien Déramond <juderamond@gmail.com>
2023-02-05 14:39:43 +0100

    Fix URL of bootstrap-logo-black.svg

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="d-flex justify-content-between align-items-md-center pb-3 mb-5 border-bottom">
     <h1 class="h4">
       <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-        <img class="d-inline-block me-2" width="40" height="32" src="https://raw.githubusercontent.com/twbs/bootstrap/main/site/static/docs/5.2/assets/brand/bootstrap-logo-black.svg" alt="Bootstrap">
+        <img class="d-inline-block me-2" width="40" height="32" src="https://raw.githubusercontent.com/twbs/bootstrap/main/site/static/docs/5.3/assets/brand/bootstrap-logo-black.svg" alt="Bootstrap">
         <span>Vue</span>
       </a>
     </h1>
