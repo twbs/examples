b450eaf7b497f6052b27fe49a6860202a858fd53
XhmikosR <xhmikosr@gmail.com>
2023-03-21 15:29:57 +0200

    vite: set dist dir one folder up (#109)

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
