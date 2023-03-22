7ed6b6306affc239103f3a6bfb5fec5c4f619031
XhmikosR <xhmikosr@gmail.com>
2023-03-22 16:32:02 +0200

    vuew: remove tilde

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,18 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: path.resolve(__dirname, 'src'),
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~bootstrap': '../../node_modules/bootstrap/'
-    }
-  },
   server: {
     port: 8080,
     hot: true
