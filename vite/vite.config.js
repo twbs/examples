3c92b60f5a12777cd79af2f24172c46efff1542b
XhmikosR <xhmikosr@gmail.com>
2023-03-22 13:52:03 +0200

    vite.config.js: remove tilde

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -5,11 +5,6 @@ export default {
   build: {
     outDir: '../dist'
   },
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
   server: {
     port: 8080,
     hot: true
