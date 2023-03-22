f8cab2a1e620530ab63d6c3153fc15b12dfa6074
XhmikosR <xhmikosr@gmail.com>
2023-03-22 13:29:38 +0200

    webpack: fix removed index.html in 93a7af0

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,24 @@
+'use strict'
+
 const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/js/main.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer:{
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
     hot: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
   module: {
     rules: [
       {
@@ -32,7 +39,7 @@ module.exports = {
               postcssOptions: {
                 plugins: function () {
                   return [
-                    require('autoprefixer')
+                    autoprefixer
                   ];
                 }
               }
