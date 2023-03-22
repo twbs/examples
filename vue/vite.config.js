b30fe958dd673060d1688ddb92e94911c19a4a85
XhmikosR <xhmikosr@gmail.com>
2023-03-22 16:20:35 +0200

    Clean up vite.config.js

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,18 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// const path = require('path')
-
 // https://vitejs.dev/config/
 export default defineConfig({
   // root: path.resolve(__dirname, 'src'),
   plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
-  //     'bootstrap': '../node_modules/bootstrap/'
-  //   }
-  // }
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -25,20 +17,4 @@ export default defineConfig({
     port: 8080,
     hot: true
   }
-
 })
-
-
-
-// export default {
-//   root: path.resolve(__dirname, 'src'),
-//   resolve: {
-//     alias: {
-//       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-//     }
-//   },
-//   server: {
-//     port: 8080,
-//     hot: true
-//   }
-// }
