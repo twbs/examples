09fe7f2bbb9d11e68d43ab3c273b7fbc039e3043
Liam <liamk15@gmail.com>
2023-07-17 14:12:20 +0100

    Attempt to use jQuery

--- vite-jquery/src/js/main.js
+++ vite-jquery/src/js/main.js
@@ -1,11 +1,17 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
+
+// Import jQuery
+import jQuery from 'jquery';
+
+// Add jQuery to the Window (see https://getbootstrap.com/docs/5.2/getting-started/javascript/#optionally-using-jquery)
+window.jQuery = jQuery;
+
 // Import only the Bootstrap components we need
 import { Popover } from 'bootstrap';
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+// Initialise the popover onload
+jQuery(function() {
+    jQuery('[data-bs-toggle="popover"]').popover();
+});
--- vite-jquery/vite.config.js
+++ vite-jquery/vite.config.js
@@ -1,11 +1,17 @@
 const path = require('path')
+import inject from "@rollup/plugin-inject";
 
 export default {
-  root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: '../dist'
-  },
-  server: {
-    port: 8080
-  }
+    plugins: [
+        inject({
+            jQuery: 'jquery', // See https://dev.to/chmich/setup-jquery-on-vite-598k
+        }),
+    ],
+    root: path.resolve(__dirname, 'src'),
+    build: {
+        outDir: '../dist'
+    },
+    server: {
+        port: 8080
+    }
 }
