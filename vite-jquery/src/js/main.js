afb82d2d1a03ad5fa1844dca0d5676a17ce9bcad
Liam <liamk15@gmail.com>
2023-07-18 10:45:26 +0100

    bootstrap 4

--- vite-jquery/src/js/main.js
+++ vite-jquery/src/js/main.js
@@ -1,11 +1,11 @@
-// Import our custom CSS
-import '../scss/styles.scss'
-
-
 // Import jQuery
 import jQuery from 'jquery';
 
+// Import our custom CSS
+import '../scss/styles.scss'
+
 // Add jQuery to the Window (see https://getbootstrap.com/docs/5.2/getting-started/javascript/#optionally-using-jquery)
+console.log('`window.jQuery = jQuery;` gets called here');
 window.jQuery = jQuery;
 
 // Import only the Bootstrap components we need
@@ -13,5 +13,6 @@ import { Popover } from 'bootstrap';
 
 // Initialise the popover onload
 jQuery(function() {
+    console.log('.popover() gets called here');
     jQuery('[data-bs-toggle="popover"]').popover();
 });
