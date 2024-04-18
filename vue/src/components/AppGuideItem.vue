8487ee3b3dd88559a464a20de31daee1ff000f22
AshBardhan <ashbardhan@gmail.com>
2024-04-18 09:49:22 +0530

    [vue] remove 'bootstrap icon' classes when SVG icon used in a flexbox container

--- vue/src/components/AppGuideItem.vue
+++ vue/src/components/AppGuideItem.vue
@@ -9,7 +9,7 @@ export default {
 
 <template>
   <li class="d-flex align-items-center mb-2">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-circle-fill me-2" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="me-2" viewBox="0 0 16 16">
       <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
     </svg>
     <a :href="href" target="_blank" rel="noopener">{{ text }}</a>
