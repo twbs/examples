226a66ffda41dfe8c21437c595d227559a578931
vicentic@immounited.com <vicentic@immounited.com>
2025-01-02 15:45:52 +0100

    Fix: Color Mode Switcher Shows wrong icon in auto mode. Root cause contributes to occasional flickering. #546 - Unifies 'auto' and `null` handling for `theme` to simplify code and avoid occasional flickering.

--- color-modes/js/color-modes.js
+++ color-modes/js/color-modes.js
@@ -8,32 +8,33 @@
   'use strict'
 
   const getStoredTheme = () => localStorage.getItem('theme')
-  const setStoredTheme = theme => localStorage.setItem('theme', theme)
-
-  const getPreferredTheme = () => {
-    const storedTheme = getStoredTheme()
-    if (storedTheme) {
-      return storedTheme
+  const setStoredTheme = theme => {
+    if (theme == null || theme === 'auto') {
+      localStorage.removeItem('theme');
+    } else {
+      localStorage.setItem('theme', theme)
     }
-
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
   const setTheme = theme => {
-    if (theme === 'auto') {
+    if (theme == null || theme === 'auto') {
       document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
     } else {
       document.documentElement.setAttribute('data-bs-theme', theme)
     }
   }
 
-  setTheme(getPreferredTheme())
+  setTheme(getStoredTheme())
 
   const showActiveTheme = (theme, focus = false) => {
     const themeSwitcher = document.querySelector('#bd-theme')
 
     if (!themeSwitcher) {
       return
+    }
+
+    if (theme == null) {
+      theme = 'auto'
     }
 
     const themeSwitcherText = document.querySelector('#bd-theme-text')
@@ -58,14 +59,14 @@
   }
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    const storedTheme = getStoredTheme()
-    if (storedTheme !== 'light' && storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
+    const storedTheme = getStoredTheme();
+    if (storedTheme == null || storedTheme === 'auto') {
+      setTheme(); // react on system theme change only in `auto` mode.
     }
   })
 
   window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
+    showActiveTheme(getStoredTheme())
 
     document.querySelectorAll('[data-bs-theme-value]')
       .forEach(toggle => {
