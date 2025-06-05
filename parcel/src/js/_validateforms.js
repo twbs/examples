8e937055357b10e24fa07c98300afcb3b5bfe99f
Benoit Lepage <workaholiks@gmail.com>
2025-06-05 11:31:26 +0200

    add carolusel products

--- parcel/src/js/_validateforms.js
+++ parcel/src/js/_validateforms.js
@@ -1,14 +1,16 @@
 // Exemple de JavaScript de démarrage pour désactiver les soumissions de formulaires en cas de champs invalides
 
 (() => {
+
   'use strict'
 
   // Récupérer tous les formulaires auxquels nous voulons appliquer des styles de validation Bootstrap personnalisés
 
   const forms = document.querySelectorAll('.needs-validation');
-
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
+  // console.log(forms);
+
     form.addEventListener('submit', event => {
 
       if (!form.checkValidity()) {
@@ -16,7 +18,17 @@
         event.stopPropagation()
       }
 
+      // si le formulaire est vide alors on retire la class was-validated
+
+
       form.classList.add('was-validated')
     }, false)
+
+    // On retire la class was-validated si le formulaire est réinitialisé
+    form.addEventListener('reset', event => {
+      // console.log('reset');
+      form.classList.remove('was-validated')
+    }, false)
+
   })
 })()
