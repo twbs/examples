7fca61cdd5507712aed400182733b393f5b3e95d
Julien Déramond <juderamond@gmail.com>
2026-04-09 07:48:01 +0200

    React Next.js: silence Sass deprecations

--- react-nextjs/next.config.js
+++ react-nextjs/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    silenceDeprecations: ['color-functions',
+      'global-builtin',
+      'import',
+      'if-function',],
+  },
 }
 
 module.exports = nextConfig
